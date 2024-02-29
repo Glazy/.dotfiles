aa9a16e7817e11aaac69d194ff64cc4b3306a710
Luke Glazebrook <Glazy@users.noreply.github.com>
2024-02-29 19:34:01 +0000

    Add TypeScript and Ruby treesitter parsers

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,13 +1,22 @@
 return {
   'nvim-treesitter/nvim-treesitter',
   build = ':TSUpdate',
-  config = function ()
+  config = function()
+    -- https://github.com/nvim-treesitter/nvim-treesitter?tab=readme-ov-file#supported-languages
     require('nvim-treesitter.configs').setup({
-      ensure_installed = { 'html', 'lua', 'vim', 'vimdoc' },
+      ensure_installed = {
+        'html',
+        'lua',
+        'vim',
+        'vimdoc',
+        'javascript',
+        'typescript',
+        'tsx',
+        'ruby',
+      },
       auto_install = true,
       highlight = { enable = true },
       indent = { enable = true },
     })
-  end
+  end,
 }
-
