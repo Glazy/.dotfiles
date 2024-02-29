753edcae8fd1cb109483968b091cda582eaf11c6
Luke Glazebrook <Glazy@users.noreply.github.com>
2024-02-29 19:15:23 +0000

    Add mini.comment setup for code commenting

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -2,12 +2,12 @@ return {
   'tpope/vim-sleuth',
   {
     'echasnovski/mini.nvim',
-    config = function ()
+    config = function()
       require('mini.ai').setup()
+      require('mini.comment').setup()
       require('mini.surround').setup()
       require('mini.pairs').setup()
       require('mini.statusline').setup()
-    end
-  }
+    end,
+  },
 }
-
