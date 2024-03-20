bc330c240db8cbe5580c35c527b6b9c1c889bca1
Luke Glazebrook <Glazy@users.noreply.github.com>
2024-03-20 01:18:01 +0000

    Add Harpoon

--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -3,10 +3,18 @@ return {
   event = 'VeryLazy',
   config = function()
     local wk = require('which-key')
-
+    
     wk.register({
       f = { name = 'Find' },
       g = { name = 'Git' },
+
+      -- NOTE: This is done to stop the Harpoon quick-jump binds from cluttering
+      -- up the WhichKey popup.
+      ["1"] = "which_key_ignore",
+      ["2"] = "which_key_ignore",
+      ["3"] = "which_key_ignore",
+      ["4"] = "which_key_ignore",
+      ["5"] = "which_key_ignore",
     }, { prefix = '<leader>' })
   end,
 }
