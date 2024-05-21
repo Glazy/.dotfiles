44b2b845f1ddd201a69c9be0ff6350727f249b3d
Luke Glazebrook <Glazy@users.noreply.github.com>
2024-05-22 00:38:38 +0100

    Remove unneeded termguicolors configuration
    Since Neoivm 0.10, termguicolors is auto-enabled if the terminal used is
    detected to support 24-bit color (https://neovim.io/doc/user/news-0.10.html)

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -2,9 +2,6 @@ return {
   'sainnhe/everforest',
   lazy = false,
   priority = 1000,
-  init = function()
-    vim.opt.termguicolors = true
-  end,
   config = function()
     vim.cmd([[colorscheme everforest]])
   end,
