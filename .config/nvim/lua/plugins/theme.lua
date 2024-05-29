ceb04be8ae2501a2364e9357310458e408b92d90
Luke Glazebrook <Glazy@users.noreply.github.com>
2024-05-29 22:28:48 +0100

    Move to Catppuccin Macchiato theme in Neovim

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -1,8 +1,9 @@
 return {
-  'sainnhe/everforest',
+  'catppuccin/nvim',
+  name = 'cappuccin',
   lazy = false,
   priority = 1000,
   config = function()
-    vim.cmd([[colorscheme everforest]])
+    vim.cmd([[colorscheme catppuccin-macchiato]])
   end,
 }
