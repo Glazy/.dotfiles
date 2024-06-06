0d19bf34ea2b6660e1ad0665e94c28c734733660
Luke Glazebrook <Glazy@users.noreply.github.com>
2024-06-06 19:51:21 +0100

    Use Harpoon add over deprecated append

--- nvim/.config/nvim/lua/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -6,26 +6,29 @@ return {
     local harpoon = require('harpoon')
     harpoon:setup()
 
-    vim.keymap.set(
-      'n',
-      '<leader>h',
-      function() harpoon.ui:toggle_quick_menu(harpoon:list()) end,
-      { desc = 'Harpoon: List' }
-    )
+    vim.keymap.set('n', '<leader>h', function()
+      harpoon.ui:toggle_quick_menu(harpoon:list())
+    end, { desc = 'Harpoon: List' })
 
-    vim.keymap.set(
-      'n',
-      '<leader>a',
-      function() harpoon:list():append() end,
-      { desc = 'Harpoon: Add '}
-    )
+    vim.keymap.set('n', '<leader>a', function()
+      harpoon:list():add()
+    end, { desc = 'Harpoon: Add ' })
 
     -- NOTE: These binds are explicitly removed from the WhichKey popup.
-    vim.keymap.set('n', '<leader>1', function() harpoon:list():select(1) end)
-    vim.keymap.set('n', '<leader>2', function() harpoon:list():select(2) end)
-    vim.keymap.set('n', '<leader>3', function() harpoon:list():select(3) end)
-    vim.keymap.set('n', '<leader>4', function() harpoon:list():select(4) end)
-    vim.keymap.set('n', '<leader>5', function() harpoon:list():select(5) end)
+    vim.keymap.set('n', '<leader>1', function()
+      harpoon:list():select(1)
+    end)
+    vim.keymap.set('n', '<leader>2', function()
+      harpoon:list():select(2)
+    end)
+    vim.keymap.set('n', '<leader>3', function()
+      harpoon:list():select(3)
+    end)
+    vim.keymap.set('n', '<leader>4', function()
+      harpoon:list():select(4)
+    end)
+    vim.keymap.set('n', '<leader>5', function()
+      harpoon:list():select(5)
+    end)
   end,
 }
-
