c06f03852bb6298eca714c0d70f9d8aadfc753aa
Luke Glazebrook <Glazy@users.noreply.github.com>
2025-03-08 16:06:49 +0000

    Update link for lsp server configs

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -59,7 +59,7 @@ return {
         require('cmp_nvim_lsp').default_capabilities()
       )
 
-      -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
+      -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md
       local servers = {
         lua_ls = {
           settings = {
